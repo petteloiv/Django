90e60f00f3ca774d9cf24378aa7356877c79e7fd
petteloiv <petteloiv@gmail.com>
2022-05-07 10:35:19 +0900

    0507_url

--- 0508_전체게시판/community/urls.py
+++ 0508_전체게시판/community/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 app_name = 'commuinty'
 urlpatterns = [
-    
+    path('create/', views.create, name='create'),
+    path('', views.reviews, name='reviews'),
+    path('<int:review_pk>/', views.detail, name='detail'),
+    path('<int:review_pk>/comments/create/', views.createcomment, name='createcomment'),
+    path('<int:review_pk>/like/',views.like, name='like'),
 ]
--- 0508_전체게시판/movies/urls.py
+++ 0508_전체게시판/movies/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from . import views
 
 app_name = 'movies'
 urlpatterns = [
-    
+    path('', views.index, name='index'),
+    path('<int:movie_pk>/', views.moviedetail, name='moviedetail'),
+    path('recommended/', views.recommended, name='recommended'),
 ]
